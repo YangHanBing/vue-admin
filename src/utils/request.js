2bb29589eb5ab36e6222b710f0695cfab87a60b7
YangHanBing <2180244540@qq.com>
2022-07-05 12:35:12 +0800

    feat(axios): axios的简单封装

---
+++ src/utils/request.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+service.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  })
+service.interceptors.response.use(
+  (res) => {
+    return res
+  },
+  (err) => {
+    return Promise.reject(err)
+  })
+export default service
