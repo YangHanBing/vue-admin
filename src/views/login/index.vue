cd54487535768fe39680ebe49e8f72ec300ff27b
YangHanBing <2180244540@qq.com>
2022-07-05 12:49:04 +0800

    feat(登录): 创建登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p @click="send">发送</p>
+  </div>
+</template>
+<script setup>
+import User from '../../api/user'
+const send = async() => {
+  const response = await User.login()
+  console.log(response)
+}
+</script>
+<style scoped lang="scss"></style>
