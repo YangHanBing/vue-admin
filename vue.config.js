45adf0a269b629e6f285cdcf8ab7af01217346e5
YangHanBing <2180244540@qq.com>
2022-07-05 11:31:04 +0800

    feat(跨域): feat(跨域): 跨域配置

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:'./',
+  // 跨域配置
   devServer:{
     open:true,
     port:8080,
